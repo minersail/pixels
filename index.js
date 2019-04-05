d76af8b278f0234cf9bce8f60b03e93fb6639acc
minersail <jkwoo1234@gmail.com>
2019-04-05 18:19:08 -0400

    edited port

--- index.js
+++ index.js
@@ -139,6 +139,6 @@ app.get("/api/id/:id", function(req, res) {
     res.json(data[parseInt(req.params.id)]);
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port 3000!');
 });
